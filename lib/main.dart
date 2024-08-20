f56ee26dd1c7be7b0b56042c887b7dcc6f8c8d5b
jhs512 <jangka512@gmail.com>
2024-08-20 10:51:35 +0900

    work

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,19 +21,21 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeMainPage extends StatelessWidget {
+class HomeMainPage extends HookWidget {
   const HomeMainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    int number = 10;
+    final number = useState(10);
 
     return Scaffold(
       body: Center(
-        child: GestureDetector(onTap: () {
-          number++;
-          print('number : $number');
-        }, child: Text('number : $number')),
+        child: GestureDetector(
+            onTap: () {
+              number.value++;
+              print('number : ${number.value}');
+            },
+            child: Text('number : ${number.value}')),
       ),
     );
   }
